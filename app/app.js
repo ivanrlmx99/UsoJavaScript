2a21823d89fd8917b763acd63138bf71adef7aa8
Ivan rl <ivanlaureano77@gmail.com>
2020-04-14 12:09:15 -0500

    se agregaron archivos

--- app/app.js
+++ app/app.js
@@ -1,1 +1,8 @@
-document.getElementById('app').innerHTML='hola endejelin';
+/*let nombre = prompt('¿Cual es tu nombre');
+let edad = prompt('cual es tu edad');
+ 
+
+document.getElementById('app').innerHTML = `Bienvenido ${nombre} de ${edad} años`;*/
+
+var nombre="estoy programando";
+console.log(nombre);
